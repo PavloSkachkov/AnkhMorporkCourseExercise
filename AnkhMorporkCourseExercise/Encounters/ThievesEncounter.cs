b227c9cc016d7d3a3af0485cfa0e0909625b633f
Pavlo Skachkov <pavlo.skachkov@valtech.com>
2021-12-07 13:02:57 +0200

    commit

--- AnkhMorporkCourseExercise/Encounters/ThievesEncounter.cs
+++ AnkhMorporkCourseExercise/Encounters/ThievesEncounter.cs
@@ -5,18 +5,18 @@ namespace AnkhMorporkCourseExercise.Encounters
 {
     class ThievesEncounter : BaseEncounter<Thief>
     {
-        private Thief thief;
+        private readonly Thief _thief;
         public ThievesEncounter(ConsoleColor color, string startLine, string endLine, Thief thief) : base(color, startLine, endLine)
         {
-            this.thief = thief;
+            _thief = thief;
         }
 
         protected override void Accept(Player player)
         {
-            if (thief.CurrentThefts < thief.AcceptableNumberOfThefts)
+            if (_thief.CurrentThefts < _thief.AcceptableNumberOfThefts)
             {
-                player.DecreaseMoneyAmount(thief.Fee);
-                thief.CurrentThefts++;
+                player.DecreaseMoneyAmount(_thief.Fee);
+                _thief.CurrentThefts++;
             }
             else
                 Console.WriteLine(" You're lucky. We did everything we wanted for today.");
